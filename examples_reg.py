71cf5768a7a6632bc9cb44374c83ab0d9c818e98
ekate <katsiarynagreb@yahoo.com>
2025-10-15 10:19:22 +0200

    first

--- examples_reg.py
+++ examples_reg.py
@@ -4,19 +4,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 try:
-    link = "https://suninjuly.github.io/registration1.html"
+    link = "https://suninjuly.github.io/registration2.html"
     browser = webdriver.Chrome()
     browser.get(link)
-    first_name = browser.find_element(By.CLASS_NAME, "first")
+    first_name = browser.find_element(By.CSS_SELECTOR, "input.first:required")
     first_name.send_keys('buba')
-    last_name = browser.find_element(By.CLASS_NAME, "second")
+    last_name = browser.find_element(By.CSS_SELECTOR, "input.second:required")
     last_name.send_keys('bubaev')
-    email = browser.find_element(By.CLASS_NAME, "third")
+    email = browser.find_element(By.CSS_SELECTOR, "input.third:required")
     email.send_keys('bubaev@frum.rum')
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
-    time.sleep(1)
+    #time.sleep(3)
 
     welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
     welcome_text = welcome_text_elt.text
